7dacb03b0019ec47135433545752c133f0d0f560
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-07-12 17:34:13 +0200

    write size as a JSON file

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,18 +1,37 @@
 /// <reference types="cypress" />
+
+const getIframesContainer = () =>
+  window.parent.window.document.querySelector('.iframes-container')
+
+const findAppRectangle = () => {
+  const el = getIframesContainer()
+  return el.getClientRects()[0]
+}
+
+// because element rectangle does not want to reveal its secrets
+const serializeRectangle = (rect) =>
+  Cypress._.pick(rect, 'bottom', 'height', 'left', 'top', 'right', 'width')
+
 it('works', () => {
   cy.visit('http://todomvc.com/examples/react/')
   cy.get('.new-todo').should('be.visible')
   cy.wait(1000)
   .then(() => {
-    console.log(window.screen)
-    const doc = cy.state('document')
-    const rect = doc.body.getClientRects()[0]
-    console.log(rect)
-    const size = Cypress._.pick(rect, 'bottom', 'height', 'left', 'top', 'right', 'width')
+    const rect = findAppRectangle()
+    const size = serializeRectangle(rect)
     console.log(size)
-    return cy.task('size', size)
+    // return cy.task('size', size)
+    const s = JSON.stringify(size, null, 2) + '\n'
+    cy.writeFile('size.json', s, 'utf8')
   })
-  cy.screenshot('app', {
-    capture: 'viewport'
+  // capture several resolutions
+  cy.screenshot('runner', {
+    capture: 'runner'
   })
+  // cy.screenshot('app', {
+  //   capture: 'viewport'
+  // })
+  // cy.screenshot('full', {
+  //   capture: 'fullPage'
+  // })
 })
