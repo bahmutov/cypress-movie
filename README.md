3e90101078471d67520e7d2bce4c888b01067cfd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-10-07 09:44:20 -0400

    fix toast injection

---
+++ README.md
@@ -0,0 +1,2 @@
+# cypress-movie
+> Exploring how one can generate product demo videos from Cypress tests
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,16 +1,28 @@
 /// <reference types="cypress" />
 
+let tinyToastSource
+
+before(function loadTinyToastSource () {
+  cy.readFile('./node_modules/tiny-toast/dist/tiny-toast.js').then(src => {
+    tinyToastSource = src
+  })
+})
+
 Cypress.Commands.add('toast', (message, duration = 3000) => {
-  cy.window({log: false}).its('tinyToast').invoke('show', message).invoke('hide', duration)
-  cy.wait(duration, {log: false})
+  cy.window({ log: false })
+    .its('tinyToast')
+    .invoke('show', message)
+    .invoke('hide', duration)
+  cy.wait(duration, { log: false })
 })
 
 const getIframesContainer = () =>
   window.parent.window.document.querySelector('.iframes-container')
 
-const loadTinyToast = (doc) => {
-  const tinyToastUrl = 'https://cdn.rawgit.com/bahmutov/tiny-toast/8ab63c4a/dist/tiny-toast.js'
+const loadTinyToast = doc => {
+  // const tinyToastUrl = 'https://cdn.rawgit.com/bahmutov/tiny-toast/8ab63c4a/dist/tiny-toast.js'
   // const doc = cy.state('document')
+
   const script_tag = doc.createElement('script')
   script_tag.type = 'text/javascript'
   script_tag.src = tinyToastUrl
@@ -23,17 +35,16 @@ const findAppRectangle = () => {
 }
 
 // because element rectangle does not want to reveal its secrets
-const serializeRectangle = (rect) =>
+const serializeRectangle = rect =>
   Cypress._.pick(rect, 'bottom', 'height', 'left', 'top', 'right', 'width')
 
-const addTodo = (text) =>
-  cy.get('.new-todo').type(`${text}{enter}`)
+const addTodo = text => cy.get('.new-todo').type(`${text}{enter}`)
 
 // scrolls back to the top
 // really necessary only if the page is taller than
 // the viewportHeight in "cypress.json"
 const scroll = () =>
-  cy.get('h1', {log: false}).scrollIntoView({
+  cy.get('h1', { log: false }).scrollIntoView({
     offset: {
       top: -100,
       left: 0
@@ -41,26 +52,32 @@ const scroll = () =>
     log: false
   })
 
-const pickFilter = (filter) => {
-  cy.contains('footer ul.filters li', filter).should('be.visible').click()
+const pickFilter = filter => {
+  cy.contains('footer ul.filters li', filter)
+    .should('be.visible')
+    .click()
   scroll()
   const hash = filter === 'All' ? '/' : `/${filter.toLowerCase()}`
   cy.location('hash').should('include', hash)
-  cy.wait(1000, {log: false})
+  cy.wait(1000, { log: false })
 }
 
 const clearCompleted = () => {
-  cy.get('footer button.clear-completed').should('be.visible').click()
+  cy.get('footer button.clear-completed')
+    .should('be.visible')
+    .click()
   cy.get('footer button.clear-completed').should('not.be.visible')
   scroll()
 }
 
 it('adds and removes completed todos', function () {
   cy.visit('http://todomvc.com/examples/react/', {
-    onBeforeLoad: (win) => {
-      loadTinyToast(win.document)
+    onBeforeLoad: win => {
+      // loadTinyToast(win.document)
+      debugger
+      win.eval(tinyToastSource)
     },
-    onLoad: (win) => {
+    onLoad: win => {
       win.tinyToast.show(this.test.title).hide(3000)
     }
   })
@@ -69,7 +86,9 @@ it('adds and removes completed todos', function () {
   addTodo('second todo')
 
   // cy.wait(1000)
-  cy.contains('ul.todo-list li', 'second todo').find('input.toggle').check()
+  cy.contains('ul.todo-list li', 'second todo')
+    .find('input.toggle')
+    .check()
   cy.toast('Checked 1 todo')
   scroll()
 
