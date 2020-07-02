580d0ade46ccc1d337350f10592f72fd731af637
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-02 09:42:39 -0400

    feat: add cy.text command (#33)
    ```js
    // all parameters are optional
    cy.text('This is some text', {
      duration: 2000, // how long the text should be there
      blocking: true, // wait for the text to hide
      textSize: '20pt', // CSS text height
    })
    ```

---
+++ src/text-overlay.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+/**
+ * Shows text overlay and then removes it.
+ */
+const textCommand = (text, options = {}) => {
+  if (!text) {
+    throw new Error('cy.text expects text')
+  }
+  if (!Cypress._.isString(text)) {
+    throw new Error('cy.text expects a string')
+  }
+
+  Cypress._.defaults(options, {
+    duration: 2000,
+    blocking: false,
+    textSize: '40pt',
+  })
+
+  const doc = cy.state('document')
+  const body = doc.body
+
+  const textHtml = `
+    <div id="text-overlay-1647"
+      style="position: absolute; left: 0; right: 0; bottom: 0;
+        text-align:center; width: 100%; padding: 4rem 0;
+        font-size: ${options.textSize}; color: white;
+        background: linear-gradient(to bottom, rgba(100, 100, 100, 0.3) 0%, rgba(10, 10, 10, 0.5) 40%, rgba(10, 10, 10, 0.5) 70%, rgba(80, 80, 80, 0.3) 100%);
+        ">
+      ${text}
+    </div>
+  `
+  Cypress.$(body).append(textHtml)
+
+  // remove text after "duration" ms
+  setTimeout(() => {
+    const textElement = doc.getElementById('text-overlay-1647')
+    if (textElement) {
+      body.removeChild(textElement)
+    }
+  }, options.duration)
+
+  if (options.blocking) {
+    cy.wait(options.duration, { log: false })
+  }
+}
+
+Cypress.Commands.add('text', textCommand)
